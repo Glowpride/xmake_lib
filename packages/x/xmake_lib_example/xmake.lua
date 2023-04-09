170ce2556a8fd8103510a7c92a5da6dbbe3d4d23
Glowpride <73988038+Glowpride@users.noreply.github.com>
2023-04-09 21:05:10 +0300

    rm first

--- packages/x/xmake_lib_example/xmake.lua
+++ packages/x/xmake_lib_example/xmake.lua
@@ -3,8 +3,8 @@ package("xmake_lib_example")
     set_urls("https://github.com/Glowpride/xmake_lib_example.git")
 
     on_install("windows", "macosx", "linux", function (package)
-        import("package.tools.xmake").install(package)
         os.rm("xmake.lua")
+        import("package.tools.xmake").install(package)
     end)
 
 package_end()
